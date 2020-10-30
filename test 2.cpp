c9253473aeba58b50a251b82e2a65cf4fe9fd58b
salman2481 <55204448+salman2481@users.noreply.github.com>
2020-10-30 14:53:10 +0500

    update

--- test 2.cpp
+++ test 2.cpp
@@ -133,11 +133,66 @@ int main()
     return 0; 
 } 
 
-echo "# hect" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/MSSW2481/hect.git
-git push -u origin main
-                
+#include<iostream>
+using namespace std;
+class node
+{
+	int value;
+	node*nextnode;
+	int size;
+	public:
+		void setvalue(int m)
+		{
+		value=m;	
+		}
+		void setnextnode(node *nn)
+		{
+			nextnode=nn;
+		}
+		int getvalue()
+		{
+			return value;
+		}
+			node *getnextnode()
+		{
+			return nextnode;
+		}
+};
+class stack
+{
+	node *top;
+	int size;
+	public:
+		void push(int m)
+		{
+		if(size==0)
+		{
+			node *newnode=new node;
+			newnode->setvalue(m);
+			top->setnextnode(0);
+			top=newnode;
+		}
+		else
+		{
+		
+					node *newnode=new node;
+					newnode->setvalue(m);
+					newnode->setnextnode(top);
+					top=newnode;
+						
+		}
+				
+			}
+		
+		int pop()
+		{
+		
+		int	x=top->getvalue();
+			node *p=top;
+			top=top->getnextnode();
+			delete p;
+			return x;
+		}
+		
+};
+
